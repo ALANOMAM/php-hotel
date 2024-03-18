f8147afe45b1e6535e816d551897ef9e39ec88bb
alan_omam <omamalan@hotmail.it>
2024-03-18 16:17:43 +0100

    parte della tabella fatta

--- index.php
+++ index.php
@@ -53,21 +53,47 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 <body>
-     
+
+<div class="container">
+
+<table class="table">
+
 <?php
 foreach($hotels as $currentHotel){
     echo "
-        <ul>
+    <thead>
+    <tr>
             ";
       foreach($currentHotel as $key => $value){
-             echo "<li>$key : $value</li>";
+             echo "<th>$key</th>";
       }
       echo "
-      </ul>   
+      </tr>
+  </thead> 
   ";
 }
-
 ?>
+
+<?php
+foreach($hotels as $currentHotel){
+    echo "
+    <tbody>
+    <tr>
+            ";
+      foreach($currentHotel as $key => $value){
+             echo "<td>$value</td>";
+      }
+      echo "
+      </tr>
+  </tbody> 
+  ";
+}
+?>
+
+</table>
+
+
+</div>
 
    <!-- bootstrap -->
    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
